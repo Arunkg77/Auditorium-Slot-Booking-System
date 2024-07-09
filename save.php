b0b8b6ee120196704c976b6eb9f5ea40d26f60b7
Arunkg77 <153452225+Arunkg77@users.noreply.github.com>
2024-07-09 10:53:29 +0530

    Update save.php

--- save.php
+++ save.php
@@ -78,8 +78,8 @@ function configureSMTP(PHPMailer $mail) {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'arunkgudagi77@gmail.com';
-    $mail->Password = 'rxlemfklgwehkifp';
+    $mail->Username = '';
+    $mail->Password = '';
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 }
